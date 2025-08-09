8f622f966854863606f6b8ccc4378f7bd3b94dbb
Andrew Gibbons <andrew.h.gibbons@gmail.com>
2025-08-09 21:57:09 +0900

    Added Error based calc ending

--- app/Main.hs
+++ app/Main.hs
@@ -8,11 +8,13 @@ ny :: Int
 nx = 100
 ny = 80
 
+eps0 :: Double
+eps0 = 1.0
+
 iterations :: Int
 iterations = 10000
 
 epsilonSim :: Array U Ix2 Double
--- epsilonSim = makeArrayR U Seq (Sz (ny :. nx)) (\(i :. _) -> if (i > 30 && i < 50) then 2.0 else 1.0)
 epsilonSim = A.replicate Seq (Sz (ny :. nx)) 1.0
 
 laplacianStencil :: (Fractional a) => Stencil Ix2 a a
@@ -35,12 +37,11 @@ gradientx = makeStencil (Sz (3 :. 3)) (1 :. 1) $ \get ->
 
 gradx :: Border Double -> Array U Ix2 Double -> Array U Ix2 Double
 gradx border arr = computeAs U $ mapStencil border gradientx arr
+{-# INLINE gradx #-}
 
 grady :: Border Double -> Array U Ix2 Double -> Array U Ix2 Double
 grady border arr = computeAs U $ mapStencil border gradienty arr
-
-eps0 :: Double
-eps0 = 1.0
+{-# INLINE grady #-}
 
 write2csv :: FilePath -> Array U Ix2 Double -> IO ()
 write2csv fp arr = do
@@ -88,40 +89,37 @@ setHBorder left right arr = computeAs U $ imap f arr
         | j == w - 1 = right
         | otherwise = x
 
--- runSim :: Int -> (Array U Ix2 Double -> Array U Ix2 Double) -> IO (Array U Ix2 Double)
-runSim :: Int -> (Array U Ix2 Double -> Array U Ix2 Double) -> IO (Array U Ix2 Double)
-runSim maxIteration runfunc = runsim' 0 runfunc phi
+runSim :: Double -> Int -> Int -> (Array U Ix2 Double -> Array U Ix2 Double) -> IO (Array U Ix2 Double)
+runSim threshold maxIteration evalStep runfunc = runsim' 0 phi
   where
     phi = A.replicate Seq (Sz (ny :. nx)) 0.0
-    runsim' n rfunc p
+    runsim' n p
         | n == maxIteration = return p
-        | n `mod` 100 == 0 = do
-            putStrLn $ "Iteration " ++ show n
-            runsim' (n + 1) rfunc (rfunc p)
-        | otherwise = runsim' (n + 1) rfunc (rfunc p)
+        | n `mod` evalStep == 0 = do
+            let p' = runfunc p
+            let simerror = calcError p p'
+            putStrLn $ "Iteration " ++ show n ++ " with error " ++ show simerror
+            if simerror < threshold then return p' else runsim' (n + 1) p'
+        | otherwise = runsim' (n + 1) (runfunc p)
+
+calcError :: Array U Ix2 Double -> Array U Ix2 Double -> Double
+calcError a1 = A.maximum' . fmap abs . A.zipWith (-) a1
 
 main :: IO ()
 main = do
-    mcharge <-
-        makeMArray
-            Seq
-            (Sz2 ny nx)
-            ( \(i :. j) ->
-                return (if i == 10 && j == 30 then 2.0 else 0.0)
-            ) ::
-            IO (MArray RealWorld P Ix2 Double)
+    mcharge <- newMArray (Sz (ny :. nx)) 0.0 :: IO (MArray RealWorld P Ix2 Double)
     write_ mcharge (40 :. 50) (-2.0)
+    write_ mcharge (20 :. 20) (-1.0)
     charge <- freeze Seq mcharge
     let charge2 = computeAs U charge
-    write2csv "charge.csv" $ computeAs U charge
-    write2csv "epsilon.csv" $ computeAs U epsilonSim
-    let phi0' = makeArray Seq (Sz (ny :. nx)) (const 0.0) :: Array D Ix2 Double
-    let resultphi = iterate (setHBorder (-1.0) (1.0) . step charge2 epsilonSim) (computeAs U phi0') !! iterations
-    write2csv "phi.csv" resultphi
+
+    resultphi <- runSim 1e-4 iterations 500 $ setHBorder (-0.5) 0.5 . setVBorder (-1.0) 1.0 . step charge2 epsilonSim
 
     let ex = gradx Wrap resultphi
         ey = grady Wrap resultphi
+
+    write2csv "phi.csv" resultphi
+    write2csv "charge.csv" $ computeAs U charge
+    write2csv "epsilon.csv" $ computeAs U epsilonSim
     write2csv "Ex.csv" ex
     write2csv "Ey.csv" ey
-
-    putStrLn $ "Finished running " ++ show iterations ++ " iterations"
